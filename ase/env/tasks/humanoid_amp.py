686b70cddb10ca106b8d7aa7bd2f7a90e62a8f33
xbpeng <JasonPeng142@hotmail.com>
2022-10-05 11:09:33 -0700

    fixing potential issues due to padding at the end of a motion

--- ase/env/tasks/humanoid_amp.py
+++ ase/env/tasks/humanoid_amp.py
@@ -97,7 +97,13 @@ class HumanoidAMP(Humanoid):
             assert(self._amp_obs_demo_buf.shape[0] == num_samples)
         
         motion_ids = self._motion_lib.sample_motions(num_samples)
-        motion_times0 = self._motion_lib.sample_time(motion_ids)
+        
+        # since negative times are added to these values in build_amp_obs_demo,
+        # we shift them into the range [0 + truncate_time, end of clip]
+        truncate_time = self.dt * (self._num_amp_obs_steps - 1)
+        motion_times0 = self._motion_lib.sample_time(motion_ids, truncate_time=truncate_time)
+        motion_times0 += truncate_time
+
         amp_obs_demo = self.build_amp_obs_demo(motion_ids, motion_times0)
         self._amp_obs_demo_buf[:] = amp_obs_demo.view(self._amp_obs_demo_buf.shape)
         amp_obs_demo_flat = self._amp_obs_demo_buf.view(-1, self.get_num_amp_obs())
